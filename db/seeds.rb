d7ba39b91a83550fabebd87a9339540c6a330734
SwanHub <jdprince555@gmail.com>
2019-08-21 10:28:48 -0600

    more policy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,52 +15,55 @@
 #   i += 1
 # end
 
-policy1 = Policy.create(title: "Healthcare", description: "Klobuchar sees Medicare-for-All as a 'possibility in the future,' but says she’s focused on more immediately achievable results. The Minnesota senator feels the path to universal health care begins by expanding the Affordable Care Act to include a public option — specifically allowing insurance customers on state exchanges to buy into Medicaid, the program that currently serves the disadvantaged.")
+policy1 = Policy.create(title: "Healthcare", description: "Warren has voiced support for the idea of Medicare-for-All")
 
-policy2 = Policy.create(title: "Criminal Justice Reform", description: "A former prosecutor who declined to file charges against nearly two dozen officers who killed suspects, Klobuchar wrote in an op-ed for CNN that 'we have finally started to acknowledge that there is racism in our criminal justice system and that we need to take action to fight it.' She has proposed creating a federal clemency advisory board, and appointing a White House post, outside of the Department of Justice, to shape policies on criminal justice reform.")
+policy2 = Policy.create(title: "Criminal Justice Reform", description: "In June, Warren released a plan to ban private prisons, stop contractors from marking up prices on commissary goods, and install an independent Prison Conditions Monitor inside Department of Justice’s Office of the Inspector General.")
 #
-# policy3 = Policy.create(title: "Taxes and Economics", description: "Biden has pledged to close $1.6 trillion in 'tax loopholes.'")
+policy3 = Policy.create(title: "Taxes and Economics", description: "Warren has the most aggressive tax-the-rich proposal in the field. Under her 'Ultra-Millionaire' wealth tax, households with a net worth of more than $50 million would be taxed 2 percent on every dollar of net worth above $50 million, and a 3 percent on every dollar of net worth above $1 billion.")
 
-policy4 = Policy.create(title: "Immigration", description: "Klobuchar supports a pathway to citizenship for most undocumented immigrants and an increase in legal immigration. She calls for reform, rather than abolition, of ICE.")
+policy4 = Policy.create(title: "Immigration", description: "In July, she delivered by unveiling a sweeping plan that includes 'remaking' ICE and Customs and Border Protection; allowing more refugees into the United States; ending the abuse of migrants and reducing detention; and decriminalizing border crossings.")
 
-policy5 = Policy.create(title: "The Climate Crisis", description: "Klobuchar co-sponsored the Green New Deal as an 'aspiration,' calling it 'a framework to jump-start a discussion.' She pledges a restoration of Obama-era climate policies, including rejoining the Paris climate accord on Day One and restoring the Clean Power Plan and Obama’s fuel-efficiency standards for vehicles. She’s also called for 'sweeping legislation' for green buildings. She has said that within her first 100 days in office she will introduce climate legislation that will put the United States on a path to net zero carbon emissions by 2050.")
+policy5 = Policy.create(title: "The Climate Crisis", description: "Warren’s 'Green Manufacturing Plan' takes inspiration from both the Apollo program and the Marshall Plan to 'develop, manufacture, and export the technology the world needs to confront the existential threat of climate change.'")
 
-# policy6 = Policy.create(title: "Identity and Social Justice", description: "Harris has released what her website describes as the 'most aggressive equal pay proposal in history.' The plan calls for companies to obtain 'Equal Pay Certification,' which involves proving through various means that they are not giving preferential treatment to men over women in regard to pay, promotions, and more.")
+# policy6 = Policy.create(title: "Identity and Social Justice", description: "In June, Moutlon unveiled a plan to review and upgrade service members who were less-than-honorably discharged because of sexual orientation or 'homosexual activity.'")
 
-policy7 = Policy.create(title: "Education", description: "Klobuchar does not support free public-college tuition, suggesting that it would take a 'magic genie' to be able to afford such a policy. Klobuchar does support free community college, better refinancing options for student debt, and an expansion of Pell grants.")
+policy7 = Policy.create(title: "Education", description: "Arguing that crushing student loan debt is reducing rates of homeownership, discouraging people from starting businesses, and forcing students to drop out before graduating, Warren has declared that she would cancel up to $50,000 in student-loan debt for 42 million Americans, almost completely wiping out educational debt for three-quarters of the country’s borrowers.")
 
-policy8 = Policy.create(title: "Family Issues", description: "Klobuchar is a co-sponsor of the American Family Act, which would send parents as much as $300 per month, per child, to defray the costs of raising a family. Klobuchar has been a vocal proponent of family-leave policies, but her own office’s policy called for women to repay the leave benefits they used.")
+policy8 = Policy.create(title: "Business and Tech Regulation", description: "In March, Warren released a plan to break up Facebook, Google, and Amazon, companies she argues have 'too much power over our economy, our society, and our democracy.'")
+
+policy9 = Policy.create(title: "Family Issues", description: "Warren introduced the Universal Child Care and Early Learning Act in June. Among other provisions, it would ensure that no family pays more than seven percent of their income for child care, create a federally funded network of child care centers, and guarantee day care workers were paid on par with public school teachers.")
 
 
-policy9 = Policy.create(title: "Foreign Policy", description: "Klobuchar has decried Trump’s 'foreign policy by tweet.' She voted with Republicans in a resolution of disapproval when Trump announced a precipitous withdrawal of American troops from Syria and Afghanistan.")
+policy10 = Policy.create(title: "Foreign Policy", description: "Last fall, before officially declaring her candidacy, Warren outlined in broad strokes her vision for a progressive foreign policy in an op-ed in Foreign Affairs. Among other goals, Warren emphasized the importance of ensuring trade negotiations are 'used to curtail the power of multinational monopolies and crack down on tax havens,' of making 'honest assessments of the full costs and risks associated with going to war,' and of committing to 'reprioritize diplomacy and reinvest in the State Department and the development agencies.'")
 
-policy10 = Policy.create(title: "Guns", description: "Klobuchar supports an assault weapons ban, universal background checks, and closing the 'boyfriend loophole,' which allows some domestic abusers to purchase guns so long as they were not married to the victim. She has said she balances her support of gun control against the interests of her sportsmen relatives.")
+policy11 = Policy.create(title: "Guns", description: "After the August shootings that left 31 dead in El Paso and Dayton, however, Warren unveiled a wide-ranging plan aimed at reducing gun violence by 80 percent.")
 
-policy11 = Policy.create(title: "Reproductive Rights", description: "Klobuchar has said she’d seek to codify Roe v. Wade in law if it were overturned by the Supreme Court, and has called the slate of anti-abortion legislation in states like Alabama and Georgia 'a violation of civil rights.' In the past, Klobuchar has spoken of seeking 'common ground' on abortions, 'making them safe and making them rare.'")
+policy12 = Policy.create(title: "Reproductive Rights", description: "Warren has issued a Congressional call to action to protect reproductive rights. She proposes ending the Hyde Amendment and for federal laws that would enshrine the protections of Roe v. Wade no matter what state you live in. In the same proposal, she calls for repealing the Trump administration’s gag rule that bars certain physicians from discussing abortion with their patients, and for an increase in funding for Title X, the federal government’s family planning programs.")
 
-# policy12 = Policy.create(title: "Government Reform and Anti-Corruption", description: "Inslee pledged not to accept any donations tied to the fossil fuel industry and has refused any corporate PAC money. He’s faced criticism from reform groups for not disavowing a single-candidate Super PAC devoted to helping his campaign.")
+policy13 = Policy.create(title: "Government Reform and Anti-Corruption", description: "Warren announced early on in her campaign that she would not court or accept contributions from big-money donors — a decision that ultimately resulted in her finance director quitting the campaign.")
 
-policy13 = Policy.create(title: "Poverty and Inequality", description: "Klobuchar wants to raise the corporate income tax from its Trump-tax-cut low of 21 percent to 25 percent as part of a plan to pay for a $650 billion infrastructure package. Klobuchar has also said that she will increase the minimum wage for federal contractors to $15 per hour within her first 100 days in office.")
+policy14 = Policy.create(title: "Poverty and Inequality", description: "Warren released a plan to invest in rural America. The plan calls for increased government investment in rural communities, including creating a 'public option for broadband'; strengthening health care and economic security; and breaking up the agribusiness corporations that have been crippling smaller farmers for years.")
 
-# policy14 = Policy.create(title: "Housing", description: "In April, Harris reintroduced to the Senate her Rent Relief Act, a bill aimed at combating rising rent costs by offering a refundable tax credit to Americans making less than $100,000 a year who spending at least 30 percent of their income on rent and utilities.")
+policy15 = Policy.create(title: "Housing", description: "Warren’s housing plan is built around the American Housing and Economic Mobility Act, which she initially introduced to the Senate in 2018. The bill would set aside $500 billion over the next 10 years to 'build, preserve, and rehab units that will be affordable to lower-income families.'")
 
-policy15 = Policy.create(title: "Voting Rights", description: "Klobuchar supports federal, automatic voter registration at age 18, a reform she says could create 22 million new voters. She has also emphasized the role of state governments in expanding voting rights.")
-#
-policy16 = Policy.create(title: "Reparations", description: "Klobuchar has called for investment in communities hurt by racism but insisted, 'it doesn’t have to be a direct pay for each person.'")
+policy16 = Policy.create(title: "Voting Rights", description: "In June, Warren released a plan to fortify federal elections. The plan calls for modernizing election machines; blanket, federal standards including same-day registration, early voting, and vote-by-mail, rather than the current state-by-state regulations; measures to end gerrymandering; and making Election Day a national holiday.")
 
-CandidatePolicy.create(candidate_id: 15, policy_id: policy1[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy2[:id])
-# CandidatePolicy.create(candidate_id: 15, policy_id: policy3[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy4[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy5[:id])
-# CandidatePolicy.create(candidate_id: 15, policy_id: policy6[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy7[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy8[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy9[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy10[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy11[:id])
-# CandidatePolicy.create(candidate_id: 15, policy_id: policy12[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy13[:id])
-# CandidatePolicy.create(candidate_id: 15, policy_id: policy14[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy15[:id])
-CandidatePolicy.create(candidate_id: 15, policy_id: policy16[:id])
+policy17 = Policy.create(title: "Reparations", description: "Warren supports offering reparations to black Americans in recognition of the economic toll of slavery.")
+
+CandidatePolicy.create(candidate_id: 23, policy_id: policy1[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy2[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy3[:id])
+CandidatePolicy.create(candidate_id: 23, policy_id: policy4[:id])
+CandidatePolicy.create(candidate_id: 23, policy_id: policy5[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy6[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy7[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy8[:id])
+CandidatePolicy.create(candidate_id: 23, policy_id: policy9[:id])
+CandidatePolicy.create(candidate_id: 23, policy_id: policy10[:id])
+CandidatePolicy.create(candidate_id: 23, policy_id: policy11[:id])
+CandidatePolicy.create(candidate_id: 23, policy_id: policy12[:id])
+CandidatePolicy.create(candidate_id: 23, policy_id: policy13[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy14[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy15[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy16[:id])
+# CandidatePolicy.create(candidate_id: 23, policy_id: policy17[:id])
