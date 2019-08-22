b235c2a583c2febc8ed0b35cadf05c59c76b8412
SwanHub <jdprince555@gmail.com>
2019-08-22 11:47:26 -0600

    duplicate policy data

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -4,17 +4,4 @@ class Policy < ApplicationRecord
   has_many :posts
   has_many :users, through: :posts
 
-
-    def self.to_csv
-      attributes = %w(id title description)
-
-      CSV.generate(headers: true) do |csv|
-        csv << attributes
-
-        all.each do |candidate|
-          csv << attributes.map{ |attr| candidate.send(attr) }
-          end
-        end
-      end
-
 end
