8a237064e389cbf6ad1d13740dd6baadc9d7faea
SwanHub <jdprince555@gmail.com>
2019-08-22 09:47:03 -0600

    added csvs for luke to seed

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -3,4 +3,18 @@ class Policy < ApplicationRecord
   has_many :candidates, through: :candidate_policies
   has_many :posts
   has_many :users, through: :posts
+
+
+    def self.to_csv
+      attributes = %w(id title description)
+
+      CSV.generate(headers: true) do |csv|
+        csv << attributes
+
+        all.each do |candidate|
+          csv << attributes.map{ |attr| candidate.send(attr) }
+          end
+        end
+      end
+
 end
