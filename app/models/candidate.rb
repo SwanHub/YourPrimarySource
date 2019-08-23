5d2b49752c87300c8c23297db7af9e55e89028ff
lukemenard <lukemenard@gmail.com>
2019-08-22 20:02:30 -0600

    ?

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -7,4 +7,29 @@ class Candidate < ApplicationRecord
   def urlify
       self.name.split(" ").join("+")
   end
+
+  def poll_date
+    @dates = []
+    self.polls.each do |poll|
+      @dates << poll[:date]
+    end
+    @dates
+  end
+
+  def poll_source
+    @sources = []
+    self.polls.each do |poll|
+      @sources << poll[:source]
+    end
+    @sources
+  end
+
+  def poll_value
+    @values = []
+    self.polls.each do |poll|
+      @values << poll[:value]
+    end
+    @values
+  end
+
 end
